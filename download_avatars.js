cec47be2dfc4ab84d9b48d1f30589624c5feef5b
Diane Kim Lim <dianekimlim@gmail.com>
2018-01-22 23:41:29 +0000

    completed printing avatar URLs

--- download_avatars.js
+++ download_avatars.js
@@ -1,23 +1,29 @@
 var request = require('request');
 var token = require('./secrets');
+var data = "";
 
 console.log('Welcome to the GitHub Avatar Downloaders!');
 
 function getRepoContributors(repoOwner, repoName, cb){
+
 	var options = {  
-	  url: `https://api.github.com/repost/${repoOwner}/${repoName}/contributors`;
+	  url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
 	  headers: {
 	  	'User-Agent': 'request'
-	  Authorization: token
-	  }
-	}
+	  },
+	  Authorization: 'token' + token.GITHUB_TOKEN
+	};
 
 	  request(options, function(err, res, body){
-	  	cb(err, body);
+	  	var data = JSON.parse(body);
+	  	cb(err, data);
 	  });
 }
 
 getRepoContributors("jquery", "jquery", function(err,result){
 	console.log("Errors: ", err);
-	console.log("Result: ", result);
+    // var b = '';
+	for (var i = 0; i < result.length; i++) {
+		console.log(result[i].avatar_url);
+	}
 });
