c6ebb24fa7588a1f006e3c9570c0d2840a618b8e
Diane Kim Lim <dianekimlim@gmail.com>
2018-01-22 22:21:13 +0000

    added header to project

--- download_avatars.js
+++ download_avatars.js
@@ -2,3 +2,20 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloaders!');
 
+function getRepoContributors(repoOwner, repoName, cb){
+	var options = {  
+	  url: `https://api.github.com/repost/${repoOwner}/${repoName}/contributors`;
+	  headers: {
+	  	'User-Agent': 'request'
+	  }
+	}
+
+	  request(options, function(err, res, body){
+	  	cb(err, body);
+	  });
+}
+
+getRepoContributors("jquery", "jquery", function(err,result){
+	console.log("Errors: ", err);
+	console.log("Result: ", result);
+});
