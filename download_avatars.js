4c59aec00494bae8fee7d2eacba991e938254676
Diane Kim Lim <dianekimlim@gmail.com>
2018-01-23 01:24:30 +0000

    added function downloadImagebyURL. Only tested hardlink example

--- download_avatars.js
+++ download_avatars.js
@@ -1,29 +1,49 @@
 var request = require('request');
 var token = require('./secrets');
+// var http = require('http-request');
+var fs = require('fs');
 var data = "";
 
-console.log('Welcome to the GitHub Avatar Downloaders!');
+// console.log('Welcome to the GitHub Avatar Downloaders!');
 
-function getRepoContributors(repoOwner, repoName, cb){
+// 	function getRepoContributors(repoOwner, repoName, cb){
 
-	var options = {  
-	  url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
-	  headers: {
-	  	'User-Agent': 'request'
-	  },
-	  Authorization: 'token' + token.GITHUB_TOKEN
+// 		var options = {  
+// 		  url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+// 		  headers: {
+// 		  	'User-Agent': 'request'
+// 		  },
+// 		  Authorization: 'token' + token.GITHUB_TOKEN
+// 		};
+
+// 		  request(options, function(err, res, body){
+// 		  	var data = JSON.parse(body);
+// 		  	cb(err, data);
+// 		  });
+// 	}
+
+// 	getRepoContributors("jquery", "jquery", function(err,result){
+// 		console.log("Errors: ", err);
+// 		for (var i = 0; i < result.length; i++) {
+// 			console.log(result[i].avatar_url);
+// 		}
+// 	});
+
+	function downloadImageByURL(url, filePath){
+		request.get(url)
+	          .on('error', function (err) {
+	            throw err;
+	          })
+		.pipe(fs.createWriteStream(filePath))
+	  
 	};
 
-	  request(options, function(err, res, body){
-	  	var data = JSON.parse(body);
-	  	cb(err, data);
-	  });
-}
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 
-getRepoContributors("jquery", "jquery", function(err,result){
-	console.log("Errors: ", err);
-    // var b = '';
-	for (var i = 0; i < result.length; i++) {
-		console.log(result[i].avatar_url);
-	}
-});
+
+
+
+
+
+
+
